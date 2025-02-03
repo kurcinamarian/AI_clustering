641f5a7f948a11f5dc5804342d9e89c661907890
kurcinamarian <marian.kurcina2003@gmail.com>
2025-02-03 20:38:18 +0100

    translated 2cc

--- 2cc.py
+++ 2cc.py
@@ -3,42 +3,42 @@ import plotly.graph_objs as go
 import math
 ##########################################################################################
 
-#globalne premenne a ich prvotna inicializacia
+#Global variables and their initial initialization
 
-#Pocet pociatocnych bodov a vsetkych bodov
-first_num = int(input("Zadaj pocet prvych bodov: "))
-total_num = int(input("Zadaj pocet vsetkych bodov: "))
+#The number of initial points and the total number of points
+first_num = int(input("Enter the number of initial points: "))
+total_num = int(input("Enter the number of total points: "))
 
-#vyber typu klastrovania
-if input("Chces klastrovat pomocou centroidu alebo medoidu?(c/m): ") == "m":
+#Select the type of clustering
+if input("Do you want to cluster using centroid or medoid?(c/m): ") == "m":
     medoid = True
 else:
     medoid = False
 
-#data o suradniciach bodov
+#data about the coordinates of points
 points_x = [0] * total_num
 points_y = [0] * total_num
 
-#pocet klastrov
+#number of clusters
 clusters_num = total_num
-#data o prvkoch v jednotlivych klastroch
+#data about the elements in individual clusters
 clusters = [[] for _ in range(total_num)]
-#data o suradniciach centra klastrov
+#data about the coordinates of the cluster centers
 clusters_x = [0] * total_num
 clusters_y = [0] * total_num
 
-#matica vsetkych vzdialenosti klastrov
+#matrix of all cluster distances
 distances = [[0.0] * total_num for _ in range(total_num)]
 
 #########################################################################################################
 
-#funkcia na vytvorenie bodov
+#function to create points
 def create_points():
     global points_x, points_y, clusters_x, clusters_y, clusters
-    # rozmery platna
+    #Canvas dimensions
     x_min, x_max = -5000, 5000
     y_min, y_max = -5000, 5000
-    #vytvorenie pociatocnych bodov
+    #Initial point creation
     for i in range(first_num):
         x = random.randint(x_min, x_max)
         y = random.randint(y_min, y_max)
@@ -47,47 +47,47 @@ def create_points():
             y = random.randint(y_min, y_max)
         points_x[i] = x
         points_y[i] = y
-    #vytvorenie ostatnych bodov
+    #Creation of the remaining points
     for i in range(first_num, total_num):
-        #vyber rodica noveho bodu
+        #Select the parent of the new point
         point_parent = random.randint(0, i - 1)
-        #vytvorenie offsetu ak je bod vzdialeny od okraja viac ako 100
+        #Create the offset if the point is more than 100 units away from the edge
         if -4900 < points_x[point_parent] < 4900:
             x_offset = random.randint(-100, 100)
-        #ak nie je vytvorenie upraveneho offsetu
+        #If the offset is not created, modify the offset accordingly
         elif points_x[point_parent] <= -4900:
             x_offset = random.randint(5000 + points_x[point_parent], 100)
         else:
             x_offset = random.randint(-100, 5000 - points_x[point_parent])
 
-        # vytvorenie offsetu ak je bod vzdialeny od okraja viac ako 100
+        #Creating an offset if the point is more than 100 units away from the edge
         if -4900 < points_y[point_parent] < 4900:
             y_offset = random.randint(-100, 100)
-        # ak nie je vytvorenie upraveneho offsetu
+        #If not, create an adjusted offset
         elif points_y[point_parent] <= -4900:
             y_offset = random.randint(5000 + points_y[point_parent], 100)
         else:
             y_offset = random.randint(-100, 5000 - points_y[point_parent])
 
-        #vytvorenie noveho bodu s offsetom od rodica
+        #Creating a new point with an offset from the parent
         points_x[i] = points_x[point_parent] + x_offset
         points_y[i] = points_y[point_parent] + y_offset
-    #kopirovanie dat bodov do dat klastrov, na zaciatku je kazdy bod vlastnym klastrom
+    #Copying point data into cluster data, where each point is initially its own cluster
     clusters_x = points_x.copy()
     clusters_y = points_y.copy()
     for i in range(clusters_num):
         clusters[i].append(i)
 
-#funkcia na vypocet pociatocnej matice vzdialenosti
+#Function to calculate the initial distance matrix
 def calculate_initial_distances():
     global distances, clusters_num, clusters_x, clusters_y
     for i in range(clusters_num):
         for j in range(i + 1, clusters_num):
-            #vypocet vzdialenosti pomocou pytagorovej vety
+            #Calculation of distance using the Pythagorean theorem
             distances[i][j] = math.sqrt((clusters_x[i] - clusters_x[j]) ** 2 + (clusters_y[i] - clusters_y[j]) ** 2)
             distances[j][i] = distances[i][j]
 
-#funkcie na najdnie najblizsich klastrov
+#Functions to find the nearest clusters
 def find_min():
     global distances, clusters_num
     min_distance = distances[0][1]
@@ -99,20 +99,20 @@ def find_min():
                 min_a, min_b = i, j
     return min_a, min_b
 
-#funkcia na kontroly podmienky: priemerna vzdialenost bodov od stredu nie je viac ako 500
+#Function to check the condition: the average distance of points from the center is not more than 500
 def average(previous, adding):
     global points_x, points_y, clusters
     combined_points = clusters[previous] + clusters[adding]
     num_points = len(combined_points)
 
-    #vypocitanie teoretickeho noveho centroidu
+    #Calculation of the theoretical new centroid
     sum_x, sum_y = 0,0
     for point in combined_points:
         sum_x += points_x[point]
         sum_y += points_y[point]
     center_x = sum_x / num_points
     center_y = sum_y / num_points
-    #ak pouzivame medoid, najdeme najblizsi bod k centroidu(medoid) sa stane centrom
+    #When using the medoid, we find the closest point to the centroid (the medoid becomes the center)
     if medoid:
         min_distance = math.sqrt((points_x[0] - center_x) ** 2 + (points_y[0] - center_y) ** 2)
         min_point = 0
@@ -123,20 +123,20 @@ def average(previous, adding):
                 min_point = point
         center_x = points_x[min_point]
         center_y = points_y[min_point]
-    #vypocet suctu vzdialenosti od centra
+    #Calculation of the sum of distances from the center
     total_distance = 0
     for point in combined_points:
         total_distance += math.sqrt((points_x[point] - center_x) ** 2 + (points_y[point] - center_y) ** 2)
-    #overenie podmienky
+    #Condition check
     return total_distance / num_points < 500
 
-#funkcia na pridanie klastru b do klastru a
+#Function to add cluster b to cluster a
 def add_cluster(a, b):
     global clusters
     clusters[a].extend(clusters[b])
     clusters[b] = []
 
-#funkcia na vypocitanie suradnic centroidu
+#Function to calculate the coordinates of the centroid
 def calculate_centroid(a):
     global points_x, points_y, clusters
     sum_x, sum_y = 0,0
@@ -146,7 +146,7 @@ def calculate_centroid(a):
         sum_y += points_y[point_index]
     return sum_x / num, sum_y / num
 
-#funkcia na zistenie suradnic medoidu, hladam bod s najmensou vzdialenostou k centroidu
+#Function to find the coordinates of the medoid, looking for the point with the smallest distance to the centroid
 def find_medoid(x,y,cluster_id):
     global points_x,points_y,clusters
     min_distance = math.sqrt((points_x[0] - x) ** 2 + (points_y[0] - y) ** 2)
@@ -158,14 +158,14 @@ def find_medoid(x,y,cluster_id):
             min_point = point
     return points_x[min_point], points_y[min_point]
 
-#funkcia sluziaca na aktualizovanie matice vzdialenosti a dat o klastroch
+#Function used to update the distance matrix and cluster data
 def update_distances(a, b):
     global distances, clusters, clusters_x, clusters_y, clusters_num
-    #vypocet novych vzdialenosti klastrov od klastru a
+    #Calculation of new distances from cluster a to other clusters
     for i in range(clusters_num):
         distances[a][i] = math.sqrt((clusters_x[a] - clusters_x[i]) ** 2 + (clusters_y[a] - clusters_y[i]) ** 2)
         distances[i][a] = distances[a][i]
-    #vymazanie informacii o klastri b pomocou jeho vymeny s poslednym platnym zaznamom
+    #Deleting information about cluster b by swapping it with the last valid entry
     clusters[b] = clusters[clusters_num - 1]
     clusters_x[b] = clusters_x[clusters_num - 1]
     clusters_y[b] = clusters_y[clusters_num - 1]
@@ -174,42 +174,42 @@ def update_distances(a, b):
         distances[i][b] = distances[i][clusters_num - 1]
     clusters_num -= 1
 
-#funkcia na spojenie dvoch klastrov
+#Function to merge two clusters
 def merge():
-    #najdenie najblizsich klastrov
+    #Finding the closest clusters
     min_a, min_b = find_min()
-    #kontrola porusenia podmienky
+    #Condition violation check
     if average(min_a, min_b):
-        #pridanie clastru b do klastru a
+        #Adding cluster b to cluster a
         add_cluster(min_a, min_b)
-        #vypocitanie centroidu
+        #Calculating the centroid
         center_x, center_y = calculate_centroid(min_a)
-        #ak pouzivame medoid, najdeme najbizsi bod k centroidu(medoid) sa stane centrom
+        #If using a medoid, find the closest point to the centroid (the medoid becomes the center)
         if medoid:
             center_x, center_y = find_medoid(center_x, center_y, min_a)
         clusters_x[min_a] = center_x
         clusters_y[min_a] = center_y
-        #aktulizovanie matice vzdialenosti a dat o klastroch
+        #Updating the distance matrix and cluster data
         update_distances(min_a, min_b)
         return True
     return False
 
-#funkcia sluziaca na vykreslenie vyslednych klasterov
+#Function used to plot the final clusters
 def draw():
     global clusters_num, clusters, points_x, points_y, clusters_x, clusters_y
-    # rozmery platna
+    #Canvas dimensions
     x_min, x_max = -5000, 5000
     y_min, y_max = -5000, 5000
     traces = []
 
-    #pre kazdy klaster si vytvorim nahodnu farbu
+    #Assigning a random color for each cluster
     colors = []
     for i in range(clusters_num):
         hue = i*10
         saturation = 70
         lightness = 50
         colors.append(f'hsl({hue}, {saturation}%, {lightness}%)')
-    #vykreslenie bodov pre kazdy klaster
+    #Plotting points for each cluster
     for cluster_id in range(clusters_num):
         cluster_points_x = []
         for i in clusters[cluster_id]:
@@ -226,7 +226,7 @@ def draw():
             showlegend=False
         )
         traces.append(trace)
-        # vykreslenie centroidov/medoidov
+        #Plotting centroids/medoids
     center_points_x = []
     for i in range(clusters_num):
         center_points_x.append(clusters_x[i])
@@ -241,27 +241,27 @@ def draw():
         showlegend=False
     )
     traces.append(trace)
-    #definovanie rozlozenia platna
+    #Defining the canvas layout
     layout = go.Layout(
         xaxis=dict(range=[x_min, x_max], title="x"),
         yaxis=dict(range=[y_min, y_max], title="y"),
-        title="Zadanie 2c"
+        title="2c"
     )
     fig = go.Figure(data=traces, layout=layout)
     fig.show()
 
 ############################################################################################
 
-#funkcia na vytvorenie bodov
+#Function to generate points
 create_points()
-#funkcia na vypocet pociatocnej matice vzdialenosti
+#Function to calculate the initial distance matrix
 calculate_initial_distances()
-#klastrovaci proces az kym nie je porusena podmienka
+#Clustering process until the condition is violated
 while merge():
     if clusters_num % 100 == 0:
         print(clusters_num)
     continue
-print("Konecny pocet klastrov je: ", clusters_num)
-#vykreslenie vysledku
+print("The final number of clusters is: ", clusters_num)
+#Plotting the result
 draw()
 
